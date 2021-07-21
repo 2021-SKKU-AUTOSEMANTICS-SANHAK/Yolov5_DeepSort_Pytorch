32d99b74f1d66205c923cc2fc32cd5833fe136f2
vanmeruso <vanmeruso@gmail.com>
2021-07-21 16:50:51 +0900

    video making

--- track.py
+++ track.py
@@ -50,8 +50,8 @@ def get_frame(i, frame):
     bucket = storage_client.bucket(bucket_id)
     select_query = (
         "SELECT camID, date_time, path FROM `{}.{}.{}` WHERE camID = {} ORDER BY date_time LIMIT 1".format(project_id,
-                                                                                                        dataset_id,
-                                                                                                        table_id, i))
+                                                                                                           dataset_id,
+                                                                                                           table_id, i))
     query_job = db_client.query(select_query)
     results = query_job.result()
     for row in results:
@@ -67,7 +67,7 @@ def get_frame(i, frame):
     cam = cv2.VideoCapture(path)
     cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    start_time = time.time()
+    print(cam.get(cv2.CAP_PROP_FPS))
     if cam.isOpened():
         while True:
             ret, img = cam.read()
@@ -77,7 +77,11 @@ def get_frame(i, frame):
             else:
                 break
         frame.put(save)
-        print(len(save))
+
+
+
+
+
     else:
         print('cannot open the vid #' + str(i))
         exit()
@@ -87,7 +91,8 @@ def get_frame(i, frame):
     #         cam.release()
     #         break
     #     frame.append(realframe)
-    print("vid {} get_frame finished".format(str(i)))
+
+
 
 def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
     # Resize and pad image while meeting stride-multiple constraints
@@ -121,7 +126,12 @@ def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scale
     img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
     return img, ratio, (dw, dh)
 
+
 palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
+
+
+
+
 
 def xyxy_to_xywh(*xyxy):
     """" Calculates the relative bounding box from absolute pixel values. """
@@ -176,11 +186,8 @@ def draw_boxes(img, bbox, identities=None, offset=(0, 0)):
                                  t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 2)
     return img
 
-"""
-    Yolo + deepsort.
-    The data required for reid are stored in return_dict, ids_per_frame_list (Queue)
-"""
-def detect(opt, dataset_list, return_dict, ids_per_frame_list, string):
+
+def detect(opt, dataset_list, return_dict, ids_per_frame_list, string, video_get):
     out, yolo_weights, deep_sort_weights, show_vid, save_vid, save_txt, imgsz, evaluate = \
         opt.output, opt.yolo_weights, opt.deep_sort_weights, opt.show_vid, opt.save_vid, \
         opt.save_txt, opt.img_size, opt.evaluate
@@ -222,9 +229,9 @@ def detect(opt, dataset_list, return_dict, ids_per_frame_list, string):
     # Run inference
     if device.type != 'cpu':
         model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
-    #Detection and Tracking
+
     while True:
-        #Wait until date the video is received.
+        print(string + 'start')
         while (dataset_list.empty()):
             time.sleep(1)
         start_time = time.time()
@@ -236,9 +243,12 @@ def detect(opt, dataset_list, return_dict, ids_per_frame_list, string):
                             max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
                             use_cuda=True)
         track_cnt = dict()
+        # print('time (init) : {}'.format(time.time() - time_init))
+        t0 = time.time()
         frame_cnt = 1
         images_by_id = dict()
         ids_per_frame = []
+        drawimage=[]
         for im0s in dataset:
             img = letterbox(im0s, 640, stride=32)[0]
             img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3 x 416 x 416
@@ -259,10 +269,14 @@ def detect(opt, dataset_list, return_dict, ids_per_frame_list, string):
             t2 = time_synchronized()
 
             # Process detections
-            for i, det in enumerate(pred):  # detections per image:
+            for i, det in enumerate(pred):  # detections per image
+                # if webcam:  # batch_size >= 1
+                #    p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
+                # else:
                 s, im0 = '', im0s
 
                 s += '%gx%g ' % img.shape[2:]  # print string
+                # save_path = str(Path(out) / Path(p).name)
 
                 if det is not None and len(det):
                     # Rescale boxes from img_size to im0 size
@@ -289,61 +303,68 @@ def detect(opt, dataset_list, return_dict, ids_per_frame_list, string):
                     confss = torch.Tensor(confs)
 
                     # pass detections to deepsort
-                    outputs, images_by_id = deepsort.update(xywhs, confss, im0, images_by_id, ids_per_frame, track_cnt, frame_cnt)
+                    outputs, images_by_id = deepsort.update(xywhs, confss, im0, images_by_id, ids_per_frame, track_cnt,
+                                                            frame_cnt)
 
+                    if len(outputs) > 0:
+                        bbox_xyxy = outputs[:, :4]
+                        identities = outputs[:, -1]
+                        #draw_boxes(im0, bbox_xyxy, identities)
+                        tlwh_bboxs = xyxy_to_tlwh(bbox_xyxy)
                 else:
                     deepsort.increment_ages()
-
+                # Print time (inference + NMS)
                 print('{}, {}/{} {}Done. ({}s)'.format(string, frame_cnt, len(dataset), s, t2 - t1))
 
+            drawimage.append(im0)
             frame_cnt += 1
-
+        print(im0.size)
+        video_get.put(drawimage)
+        video_get.put(track_cnt)
         return_dict.put(images_by_id)
         ids_per_frame_list.put(ids_per_frame)
+        print(string + ' Tracking Done')
 
-"""
-    Reid
-    return_dict1 : images_by_id from camera 1
-    return_dict2 : images_by_id from camera 2
-    ids_per_frame1_list : ids_per_frame from camera 1
-    ids_per_frame2_list : ids_per_frame from camera 2
-"""
-def re_identification(return_dict1, return_dict2, ids_per_frame1_list, ids_per_frame2_list):
+
+def re_identification(return_dict1, return_dict2, ids_per_frame1_list, ids_per_frame2_list, video_get1, video_get2):
     reid = REID()
+    count = 0
     while True:
-        while (return_dict1.empty()) or (return_dict2.empty()) or (ids_per_frame1_list.empty()) or (ids_per_frame2_list.empty()):
-            time.sleep(1)
+        while (return_dict1.empty()) or (return_dict2.empty()) or (ids_per_frame1_list.empty()) or ids_per_frame2_list.empty():
+                time.sleep(1)
         start_time = time.time()
         return_list = return_dict1.get()
         return_list2 = return_dict2.get()
+
 
         ids_per_frame1 = ids_per_frame1_list.get()
         ids_per_frame2 = ids_per_frame2_list.get()
         threshold = 320
         exist_ids = set()
         final_fuse_id = dict()
+        ids_per_frame = []
         ids_per_frame22 = []
+        images_by_id = dict()
         feats = dict()
         size = len(return_list)
         for key, value in return_list2.items():
             return_list[key + size] = return_list2[key]
-
         images_by_id = copy.deepcopy(return_list)
         print(len(images_by_id))
 
         for i in ids_per_frame2:
-          d = set()
-          for k in i:
-            k += size
-            d.add(k)
-          ids_per_frame22.append(d)
+            d = set()
+            for k in i:
+                k += size
+                d.add(k)
+            ids_per_frame22.append(d)
 
         ids_per_frame = copy.deepcopy(ids_per_frame1)
         for k in ids_per_frame22:
-          ids_per_frame.append(k)
+            ids_per_frame.append(k)
 
         for i in images_by_id:
-            feats[i] = reid._features(images_by_id[i])  # reid._features(images_by_id[i][:min(len(images_by_id[i]),100)])
+            feats[i] = reid._features(images_by_id[i])
 
         for f in ids_per_frame:
             if f:
@@ -375,14 +396,63 @@ def re_identification(return_dict1, return_dict2, ids_per_frame1_list, ids_per_f
                         dis.sort(key=operator.itemgetter(1))
                         if dis[0][1] < threshold:
                             combined_id = dis[0][0]
-                            images_by_id[combined_id] += images_by_id[nid]  # images_by_id[combined_id] += images_by_id[nid]
+                            images_by_id[combined_id] += images_by_id[nid]
                             final_fuse_id[combined_id].append(nid)
                         else:
                             final_fuse_id[nid] = [nid]
 
         print('Final ids and their sub-ids:', final_fuse_id)
         print('people : ', len(final_fuse_id))
-        print(time.time() - start_time)
+        #print(time.time() - start_time)
+
+        drawimage = video_get1.get()  # list
+        size2 = len(drawimage)
+        track_cnt1 = video_get1.get()  # dict plus id 해야됨
+        imag2 = video_get2.get()
+        track_cnt2 = video_get2.get()
+        for a in imag2:
+            drawimage.append(a)
+        for key, value in track_cnt2.items():
+            for a in range(len(track_cnt2[key])):
+                track_cnt2[key][a][0] +=size2
+            track_cnt1[key + size] = track_cnt2[key]
+
+        output = str(count) + '.avi'
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter(output, fourcc, 7.5, (640, 480))
+
+        for frame in range(len(drawimage)):
+           img = drawimage[frame]
+           for idx in final_fuse_id:
+             for i in final_fuse_id[idx]: #i = id
+                for f in track_cnt1[i]:
+                    if frame == f[0]:
+                        text_scale, text_thickness, line_thickness = get_FrameLabels(img)
+                        cv2_addBox(idx, img, f[1], f[2], f[3], f[4], line_thickness, text_thickness, text_scale)
+           out.write(img)
+        print("finsinh")
+        out.release()
+        count = count + 1
+
+
+
+
+def get_FrameLabels(frame):
+    text_scale = max(1, frame.shape[1] / 1600.)
+    text_thickness = 1 if text_scale > 1.1 else 1
+    line_thickness = max(1, int(frame.shape[1] / 500.))
+    return text_scale, text_thickness, line_thickness
+
+def cv2_addBox(track_id, frame, x1, y1, x2, y2, line_thickness, text_thickness, text_scale):
+    color = get_color(abs(track_id))
+    cv2.rectangle(frame, (x1, y1), (x2, y2), color=color, thickness=line_thickness)
+    cv2.putText(frame, str(track_id), (x1, y1 + 30), cv2.FONT_HERSHEY_PLAIN, text_scale, (0, 0, 255),
+                thickness=text_thickness)
+def get_color(idx):
+    idx = idx * 3
+    color = ((37 * idx) % 255, (17 * idx) % 255, (29 * idx) % 255)
+    return color
+
 
 warnings.filterwarnings('ignore')
 
